02cab47848915eb757aec9987bc3e8043c7b32ff
starling <glauco.engenharia@bithealth.com.br>
2024-06-04 15:22:43 -0300

    feat: Comand and Job to Import Product

--- app/Console/Commands/Products/Import.php
+++ app/Console/Commands/Products/Import.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands\Products;
 
+use App\Jobs\Product\CreateJob;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class Import extends Command
 {
@@ -10,8 +13,62 @@ class Import extends Command
 
     protected $description = 'Import products from an external API';
 
-    public function handle()
+    protected $request = Http::baseUrl('https://fakestoreapi.com');
+
+    public function handle(): void
     {
-        //
+        $externalIds = $this->option('id');
+        
+        if (!empty($externalIds)) {
+            foreach ($externalIds as $externalId) {
+                $this->byId($externalId);
+            }
+        } else {
+            $this->all();
+        }
+    }
+
+    private function all(): void
+    {
+        try {
+            $response = $this->request->get('products');
+        } catch (\Exception $e) {
+            Log::error('Console\Commands\Products\Import message: '.$e->getMessage(), [
+                'exception' => $e,
+            ]);
+
+            $this->error('Error communicating with external API.');
+        }
+
+        $products = $response->json();
+
+        foreach ($products as $productData) {
+            CreateJob::dispatch($productData);
+        }
+
+        $this->info('All products have been imported successfully.');
+    }
+
+    private function byId(int $externalId): void
+    {
+        try {
+            $response = $this->request->get('products/'.$externalId);
+        } catch (\Exception $e) {
+            Log::error('Console\Commands\Products\Import->byId message: '.$e->getMessage(), [
+                'exception' => $e,
+            ]);
+            
+            $this->error('Error communicating with external API.');
+        }
+        
+        $productData = $response->json();
+
+        if ($productData) {
+            CreateJob::dispatch($productData);
+
+            $this->info("Product ID: {$externalId} has been imported successfully.");
+        } else {
+            $this->error("Product ID: {$externalId} not found.");
+        }
     }
 }
--- app/Jobs/Product/CreateJob.php
+++ app/Jobs/Product/CreateJob.php
@@ -2,24 +2,42 @@
 
 namespace App\Jobs\Product;
 
+use App\Models\Product;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class CreateJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct()
+    protected array $data;
+
+    public function __construct(array $data)
     {
-        //
+        $this->data = $data;
     }
 
     public function handle(): void
     {
-        //
+        try {
+            Product::updateOrCreate(
+                ['name' => $this->data['title']],
+                [
+                    'price' => $this->data['price'],
+                    'description' => $this->data['description'],
+                    'category' => $this->data['category'],
+                    'image_url' => $this->data['image']
+                ]
+            );
+        } catch (\Exception $e) {
+            Log::error('Jobs\Product\CreateJob message: '.$e->getMessage(), [
+                'exception' => $e,
+            ]);
+            throw $e;
+        }
     }
 }
